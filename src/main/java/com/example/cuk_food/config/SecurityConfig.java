4fe04cfbe4b5f3db7b71a90f43880ee3ff41cf15
김정호 <jhjhjh1919@daum.net>
2024-05-09 20:18:21 +0900

    회원가입 -기본

--- src/main/java/com/example/cuk_food/config/SecurityConfig.java
+++ src/main/java/com/example/cuk_food/config/SecurityConfig.java
@@ -1,25 +1,19 @@
 package com.example.cuk_food.config;
 
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 public class SecurityConfig {
 
-    //인증과 권한 체크
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .authorizeHttpRequests(requests -> requests
-
                         .anyRequest().permitAll()
                 )
                 .formLogin(form -> form
@@ -28,8 +22,14 @@ public class SecurityConfig {
                         .permitAll()
                 )
                 .logout(logout -> logout
-                        .permitAll());
+                        .permitAll()
+                );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
